661455ae873948e1e14e5d998b0eb9929378f9a5
causeway-rms <118050291+causeway-rms@users.noreply.github.com>
2023-01-21 12:00:45 +0530

    Add function to play single round of Rock, Paper, Scissors

--- script.js
+++ script.js
@@ -1,16 +1,75 @@
 const gameOptions = ['rock', 'paper', 'scissors'];
 
 let computerSelection;
-let userSelection;
+let playerSelection;
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     computerSelection = gameOptions[Math.floor(Math.random()*3)];
+    console.log('Computer plays: ' + computerSelection);
     return computerSelection;
 }
 
 function getUserChoice() {
-    userSelection = prompt('Enter your choice: rock, paper, or scissors')
-    return userSelection;
+    playerSelection = prompt('Enter your choice: rock, paper, or scissors')
+    playerSelection = playerSelection.toLowerCase();
+    console.log('You play: ' + playerSelection);
+    return playerSelection;
 }
 
-console.log(getUserChoice());
+function singleRound(humanChoice, computerChoice) {
+    if (humanChoice === 'rock') {
+        if (computerChoice === 'rock') {
+            humanScore = humanScore + 0;
+            computerScore = computerScore + 0;
+            return ('It is a tie. You both played Rock.');
+        }
+        else if (computerChoice === 'paper') {
+            humanScore = humanScore + 0;
+            computerScore = computerScore + 1;
+            return ('Computer wins! Paper beats Rock!');
+        }
+        else {
+            humanScore = humanScore + 1;
+            computerScore = computerScore + 0;
+            return ('You win! Rock beats Scissors');
+        }
+    }
+    else if (humanChoice === 'paper') {
+        if (computerChoice === 'rock') {
+            humanScore = humanScore + 1;
+            computerScore = computerScore + 0;
+            return ('You win! Paper beats Rock!');
+        }
+        else if (computerChoice === 'paper') {
+            humanScore = humanScore + 0;
+            computerScore = computerScore + 0;
+            return ('It is a tie. You both plated Paper.');
+        }
+        else {
+            humanScore = humanScore + 0;
+            computerScore = computerScore + 1;
+            return ('Computer wins! Scissors beat Paper!');
+        }
+    }
+    else {
+        if (computerChoice === 'rock') {
+            humanScore = humanScore + 0;
+            computerScore = computerScore + 1;
+            return ('Computer wins! Rock beats Scissors!');
+        }
+        else if (computerChoice === 'paper') {
+            humanScore = humanScore + 1;
+            computerScore = computerScore + 0;
+            return ('You win! Scissors beat Paper!');
+        }
+        else {
+            humanScore = humanScore + 0;
+            computerScore = computerScore + 0;
+            return ('It is a tie. You both played Scissors.')
+        }
+    }
+    }
+
+console.log(singleRound(getUserChoice(),getComputerChoice()));
